bf82b7b1f18c0c1b1d203544e608dcfbcf8122a0
doangiabao645 <2124802010645@student.tdmu.edu.vn>
2023-10-22 09:24:35 +0700

    project

---
+++ SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Provider;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SachOnline.Models;
+
+
+namespace SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        private string connection;
+        private dbSachOnlineDataContext data;
+
+        public SachOnlineController()
+        {
+            // Khởi tạo chuỗi kết nối
+            connection = "Data Source=LAPTOP-O5R6LPBI;Initial Catalog=SachOnline;Integrated Security=True";
+            data = new dbSachOnlineDataContext(connection);
+        }
+        public ActionResult NavPartial()
+        {
+            return PartialView();
+        }
+        private List<SACH> LaySachMoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            //Lay 6 quyen sach moi
+            var listSachMoi = LaySachMoi(6);
+            return View(listSachMoi);
+        }
+        // GET: SachOnline
+
+        public ActionResult SachBanNhieuPartial()
+        {
+            // Truy vấn cơ sở dữ liệu để lấy danh sách sách bán nhiều nhất.
+            var listSachBanNhieu = data.SACHes.OrderByDescending(a => a.SoLuongBan).Take(6).ToList();
+
+            // Trả về PartialView chứa danh sách sách bán nhiều nhất.
+            return PartialView("SachBanNhieuPartial", listSachBanNhieu);
+        }
+
+        public ActionResult ChuDePartial()
+        {
+            var listChuDe = from cd in data.CHUDEs select cd;
+            return PartialView(listChuDe);
+        }
+        public ActionResult NhaXuatBanPartial()
+        {
+            var listNhaXuatBan = from cd in data.NHAXUATBANs select cd;
+            return PartialView(listNhaXuatBan);
+        }
+
+        // Lab3
+        public ActionResult SachTheoChuDe(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+        public ActionResult SachTheoNhaXuatBan(int id)
+        {
+            var sach = from s in data.SACHes where s.MaNXB == id select s;
+            return View(sach);
+        }
+        public ActionResult ChiTietSach(int id)
+        {
+            var sach = from s in data.SACHes
+                       where s.MaSach == id
+                       select s;
+            return View(sach.Single());
+        }
+        public ActionResult LoginLogout()
+        {
+            return PartialView("LoginLogoutPartial");
+        }
+
+    }
+}
---
+++ SachOnline/Controllers/UserController.cs
@@ -0,0 +1,134 @@
+﻿using SachOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace SachOnline.Controllers
+{
+
+    public class UserController : Controller
+    {
+        private string connection;
+        private dbSachOnlineDataContext db;
+
+        public UserController()
+        {
+            // Khởi tạo chuỗi kết nối
+            connection = "Data Source=LAPTOP-O5R6LPBI;Initial Catalog=SachOnline;Integrated Security=True";
+            db = new dbSachOnlineDataContext(connection);
+        }
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Dangky(FormCollection collection, KHACHHANG kh)
+        {
+
+
+            //Gan cac gia tri nguoi dung nhap du lieu cho cac bien
+            var sHoTen = collection["HoTen"];
+            var stenDN = collection["TenDN"];
+            var sMatkhau = collection["Matkhau"];
+            var sMatkhauNhapLai = collection["MatkhauNL"];
+            var sDiachi = collection["DiaChi"];
+            var sEmail = collection["Email"];
+            var sDienThoai = collection["DienThoai"];
+            var dNgaySinh = String.Format("{0:MM/dd/yyyy}", collection["NgaySinh"]);
+            if (String.IsNullOrEmpty(sHoTen))
+            {
+                ViewData["err1"] = "Họ tên không được rỗng";
+            }
+            else if (String.IsNullOrEmpty(stenDN))
+            {
+                ViewData["err2"] = "Tên đăng nhập không được rỗng";
+            }
+            else if (string.IsNullOrEmpty(sMatkhau))
+            {
+                ViewData["err3"] = "Phải nhập mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(sMatkhauNhapLai))
+            {
+                ViewData["err4"] = "Phải nhập lại mật khẩu";
+            }
+            else if (sMatkhau != sMatkhauNhapLai)
+            {
+                ViewData["err4"] = "Mật khẩu nhập lại không khớp";
+            }
+            else if (String.IsNullOrEmpty(sEmail))
+            {
+                ViewData["err5"] = "Email không được rỗng";
+            }
+            else if (String.IsNullOrEmpty(sDienThoai))
+            {
+                ViewData["err6"] = "Số điện thoại không được rỗng";
+            }
+            else if (db.KHACHHANGs.SingleOrDefault(n => n.TaiKhoan == stenDN) != null)
+            {
+                ViewBag.ThongBao = "Tên đăng nhập đã tồn tại";
+            }
+            else if (db.KHACHHANGs.SingleOrDefault(n => n.Email == sEmail) != null)
+            {
+                ViewBag.ThongBao = "Email đã được sử dụng";
+            }
+            else
+            {
+                //Gần giá trị cho đối tượng được tạo mới (kh)
+                kh.HoTen = sHoTen;
+                kh.TaiKhoan = stenDN;
+                kh.MatKhau = sMatkhau;
+                kh.Email = sEmail;
+                kh.DiaChi = sDiachi;
+                kh.DienThoai = sDienThoai;
+                kh.NgaySinh = DateTime.Parse(dNgaySinh);
+                db.KHACHHANGs.InsertOnSubmit(kh);
+                db.SubmitChanges();
+                return RedirectToAction("DangNhap");
+            }
+            return this.Dangky();
+        }
+        [HttpGet]
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection collection)
+        {
+            var sTenDN = collection["TenDN"];
+            var sMatKhau = collection["MatKhau"];
+            if (String.IsNullOrEmpty(sTenDN))
+            {
+                ViewData["Err1"] = "Bạn chưa nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(sMatKhau))
+            {
+                ViewData["Err2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                KHACHHANG kh = db.KHACHHANGs.SingleOrDefault(n => n.TaiKhoan == sTenDN && n.MatKhau == sMatKhau);
+                if (kh != null)
+                {
+                    ViewBag.ThongBao = "Chúc mừng đăng nhập thành công";
+                    Session["TaiKhoan"] = kh;
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                }
+            }
+            return View();
+        }
+    }
+}
